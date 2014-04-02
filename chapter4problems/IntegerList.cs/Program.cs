5a1ac114598a64afbdff6ae68fb2c144d35b3cf4
Brandon24 <brandonreinert24@gmail.com>
2014-04-02 09:48:02 -0400

    gfhghj,jhhjmvh,kjvhjb

---
+++ chapter4problems/IntegerList.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegerList.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = new int[5] { 5, 6, 7, 8, 9 };
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                Array.Sort(integers);
+                Array.Reverse(integers);
+                Console.WriteLine("{0}", integers[i]);
+            }
+        }
+    }
+}
